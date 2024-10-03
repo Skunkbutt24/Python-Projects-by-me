52804823ff2338a704bab4bc3c826a06f2170e3c
Liman <156384174+Skunkbutt24@users.noreply.github.com>
2024-10-03 13:08:59 -0400

    Shopping Cart.py

--- Shopping Cart.py
+++ Shopping Cart.py
@@ -13,29 +13,29 @@ logo = '''
 '''
 print(logo)
 print("Welcome to my virtual shopping cart program. Let's get started.")
-foods = []
+products = []
 prices = []
 total_price = 0
 number_of_items=0
 while True:
-    food = input("Enter the name of food you want to store in cart: ").lower()
-    if food == "stop":
-        print("Thanks for using my program. Bye !!!!!")
+    product = input("Enter the name of product you want to store in cart: ").lower()
+    products.append(product)
+    price = float(input(f"Enter the price of {product.capitalize()}: $ "))
+    prices.append(price)
+    print(f'Product "{product.capitalize()}" has been added to your cart for {price:.2f}$')
+    continue_adding = input("Do you want to add more stuffs ?\n").lower()
+    while continue_adding not in ["yes","no"]:
+        continue_adding=input("Invalid input. Try again.\nDo you want to add more stuffs ?\n").lower()
+    if continue_adding == "no":
         break
-    else:
-        foods.append(food)
-        price = float(input(f"Enter the price of {food.capitalize()}: $ "))
-        prices.append(price)
-        continue_adding = input("Do you want to add more stuffs ? ").lower()
-        if continue_adding == "no":
-            break
 print("So, your shopping cart contains: ")
+print("===========================================\n")
 n=1
-for food in foods:
-    print(f"{n}:{food.capitalize()}")
-    n+=1
-for price in prices:
+for product,price in zip(products,prices):
+    print(f"{n}.{product.capitalize()}: {price:.2f}$")
     total_price += price
     number_of_items+=1
-print(f"Number of items in cart: {number_of_items}")
-print(f"Total price: ${total_price:.2f}")
+    n+=1
+print(f"\nNumber of items in cart: {number_of_items}")
+print("Tax percentage: 4%")
+print(f"Total price with tax: ${(total_price+(total_price*(4/100))):.2f}")
